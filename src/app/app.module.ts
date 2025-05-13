e1f50d2c86520bea1d309bd6cfcb5a248b3a5343
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-13 16:20:52 -0300

    CreateUser: starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from '@/user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.local'],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
