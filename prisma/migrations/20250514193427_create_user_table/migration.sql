1e08e640b8100154b03bb8d5ba14bf27eddb6302
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-14 16:37:19 -0300

    feat: create user-table

---
+++ prisma/migrations/20250514193427_create_user_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "bio" TEXT,
+    "profileImg" TEXT,
+    "banner" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
