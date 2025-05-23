06064be850af67925886f622255df5e4ae37b5e6
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-23 15:49:35 -0300

    feat: creating user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -10,23 +11,51 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'Name is required' })
   @MinLength(3, { message: 'Name must be at least 3 characters long' })
   @MaxLength(50, { message: 'Name must be at most 20 characters long' })
+  @ApiProperty({
+    description: 'User name',
+    example: 'John Doe',
+    minLength: 3,
+    maxLength: 50,
+  })
   name: string;
 
   @IsNotEmpty({ message: 'Username is required' })
   @MinLength(3, { message: 'Username must be at least 3 characters long' })
   @MaxLength(50, { message: 'Username must be at most 20 characters long' })
+  @ApiProperty({
+    description: 'User username',
+    example: 'jDoe',
+    minLength: 3,
+    maxLength: 50,
+  })
   username: string;
 
   @IsNotEmpty({ message: 'Name is required' })
   @IsEmail()
+  @ApiProperty({
+    description: 'User email',
+    example: 'jonh@example.com',
+  })
   email: string;
 
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
   @MaxLength(50, { message: 'Password must be at most 20 characters long' })
+  @ApiProperty({
+    description: 'User password',
+    example: 'password',
+    minLength: 6,
+    maxLength: 50,
+  })
   password: string;
 
   @IsOptional()
   @MaxLength(200, { message: 'Bio must be at most 200 characters long' })
+  @ApiProperty({
+    description: 'User bio',
+    example: "John Doe's bio",
+    minLength: 3,
+    maxLength: 50,
+  })
   bio?: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { BcryptService } from '@/auth/bcrypt.service';
+import { PrismaService } from '@/prisma/prisma.service';
+
+type CheckUserParams = {
+  username?: string;
+  email?: string;
+};
 
 @Injectable()
 export class UserService {
-  constructor(private readonly bcryptService: BcryptService) {}
+  constructor(
+    private readonly bcryptService: BcryptService,
+    private readonly prisma: PrismaService,
+  ) {}
   async create(createUserDto: CreateUserDto) {
-    const password = await this.bcryptService.hash(createUserDto.password);
+    if (
+      await this.isUserOrEmailTaken({
+        username: createUserDto.username,
+        email: createUserDto.email,
+      })
+    ) {
+      throw new BadRequestException({
+        d: 'User or email already taken',
+      });
+    }
+    const { email, name, username, bio, password } = createUserDto;
+    const hashedPassword = await this.bcryptService.hash(password);
 
-    return { password };
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        bio,
+        password: hashedPassword,
+      },
+    });
+
+    return { user };
   }
 
   findAll() {
@@ -20,11 +49,27 @@ export class UserService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  private async isUserOrEmailTaken(params: CheckUserParams): Promise<boolean> {
+    const conditions: any[] = [];
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    if (params.username) {
+      conditions.push({ username: params.username });
+    }
+
+    if (params.email) {
+      conditions.push({ email: params.email });
+    }
+
+    if (conditions.length === 0) {
+      return false;
+    }
+
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: conditions,
+      },
+    });
+
+    return !!existingUser;
   }
 }
