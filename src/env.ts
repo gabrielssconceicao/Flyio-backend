94c88464d9eccbae99d1ab10a3a13fd0d2eacfdc
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-05-27 09:22:46 -0300

    test: testing auth.cotroller signIn route

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,24 @@
 import { z } from 'zod';
+import { config } from 'dotenv';
 
+// if (process.env.NODE_ENV === 'test') {
+//   config({ path: '.env.test' });
+// } else {
+//   config();
+// }
+config();
 const envSchema = z.object({
   APP_PORT: z.string().default('3000'),
-  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  NODE_ENV: z
+    .enum(['development', 'production,', 'test'])
+    .default('development'),
   JWT_SECRET: z.string(),
   JWT_ACCESS_TOKEN_EXPIRES_IN: z.string(),
   JWT_REFRESH_TOKEN_EXPIRES_IN: z.string(),
   JWT_TOKEN_AUDIENCE: z.string().url(),
   JWT_TOKEN_ISSUER: z.string().url(),
-  COOKIE_SECRET: z.string(),
 });
-
 const _env = envSchema.safeParse(process.env);
-
 if (!_env.success) {
   throw new Error('Invalid environment variables');
 }
