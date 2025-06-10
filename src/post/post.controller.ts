fe9bb2d45ff20e92c1b77c2cbb58d97dcd85f701
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-06-10 09:11:07 -0300

    feat: get a post, and search for posts

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -5,24 +5,26 @@ import {
   Get,
   Param,
   Post,
+  Query,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiCookieAuth } from '@nestjs/swagger';
 import * as multer from 'multer';
 import { CurrentUser } from '@/common/params/current-user.params';
 import { JwtPayload } from '@/common/interfaces/jwt-payload.interface';
 import { JwtAuthGuard } from '@/common/guard/jwt-auth.guard';
+import { QueryParamDto } from '@/common/dto/query-param.dto';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { ApiCookieAuth } from '@nestjs/swagger';
 
+@ApiCookieAuth('access_token')
+@UseGuards(JwtAuthGuard)
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @ApiCookieAuth('access_token')
-  @UseGuards(JwtAuthGuard)
   @UseInterceptors(
     FilesInterceptor('images', 4, {
       storage: multer.memoryStorage(),
@@ -36,15 +38,18 @@ export class PostController {
     return this.postService.create({ createPostDto, payload });
   }
 
-  @ApiCookieAuth('access_token')
-  @UseGuards(JwtAuthGuard)
   @Delete(':postId')
   delete(@Param('postId') postId: string, @CurrentUser() payload: JwtPayload) {
     return this.postService.delete({ postId, payload });
   }
 
   @Get(':postId')
-  findOne(@Param('postId') postId: string) {
-    return this.postService.findOne({ postId });
+  findOne(@Param('postId') postId: string, @CurrentUser() payload: JwtPayload) {
+    return this.postService.findOne({ postId, payload });
+  }
+
+  @Get()
+  findAll(@CurrentUser() payload: JwtPayload, @Query() query: QueryParamDto) {
+    return this.postService.findAll({ payload, query });
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,7 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { JwtPayload } from '@/common/interfaces/jwt-payload.interface';
+import { QueryParamDto } from '@/common/dto/query-param.dto';
 
 type CreatePost = {
   createPostDto: CreatePostDto;
@@ -11,6 +12,11 @@ type CreatePost = {
 type DeletePost = {
   payload: JwtPayload;
   postId: string;
+};
+
+type FindAll = {
+  payload: JwtPayload;
+  query: QueryParamDto;
 };
 
 @Injectable()
@@ -61,7 +67,7 @@ export class PostService {
     return;
   }
 
-  async findOne({ postId }: { postId: string }) {
+  async findOne({ postId, payload }: { postId: string; payload: JwtPayload }) {
     const post = await this.prisma.post.findUnique({
       where: {
         id: postId,
@@ -77,12 +83,91 @@ export class PostService {
             profileImg: true,
           },
         },
+        likes: {
+          where: {
+            userId: payload?.id,
+          },
+
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
       },
     });
 
     if (!post) {
       throw new NotFoundException('Post not found');
     }
-    return { post };
+
+    const { _count, likes, ...restPost } = post;
+    return {
+      ...restPost,
+      ...this.separate({ _count, likes }),
+    };
+  }
+
+  async findAll({ payload, query }: FindAll) {
+    const { search = '', limit = 50, offset = 0 } = query;
+
+    const posts = await this.prisma.post.findMany({
+      where: {
+        text: {
+          contains: search,
+          mode: 'insensitive',
+        },
+      },
+      select: {
+        id: true,
+        text: true,
+        author: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            profileImg: true,
+          },
+        },
+        likes: {
+          where: {
+            userId: payload.id,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+      },
+      take: limit,
+      skip: offset,
+    });
+
+    const count = await this.prisma.post.count();
+
+    return {
+      count,
+      posts: posts.map(({ _count, likes, ...post }) => {
+        return { ...post, ...this.separate({ _count, likes }) };
+      }),
+    };
+  }
+
+  private separate({
+    _count,
+    likes,
+  }: {
+    likes: { userId: string }[];
+    _count: { likes: number };
+    userId?: string;
+  }) {
+    return {
+      likes: _count.likes,
+      isLiked: !!likes.length,
+    };
   }
 }
