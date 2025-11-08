394c56646aa0eb811a07fb90611e1c848f1fa202
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-11-08 17:42:33 -0300

    feat: create post entity

---
+++ src/domain/social/enterprise/entities/post.ts
@@ -0,0 +1,43 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+
+export interface PostProps {
+  author_id: UniqueEntityId;
+  content: string;
+  created_at: Date;
+  updated_at?: Date;
+}
+
+export class Post extends Entity<PostProps> {
+  get author_id() {
+    return this.props.author_id;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+
+  get updated_at() {
+    return this.props.updated_at;
+  }
+
+  private touch() {
+    this.props.updated_at = new Date();
+  }
+
+  static create(props: Optional<PostProps, 'created_at'>, id?: UniqueEntityId) {
+    const post = new Post(
+      {
+        ...props,
+        created_at: props.created_at ?? new Date(),
+      },
+      id,
+    );
+    return post;
+  }
+}
