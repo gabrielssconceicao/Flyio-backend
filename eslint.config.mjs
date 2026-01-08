795ebf383eb51c66fa5f39c1387d93c3e311eba4
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-01-08 11:09:02 -0300

    chore: add vitest, eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
@@ -12,6 +12,9 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     languageOptions: {
       globals: {
         ...globals.node,
@@ -26,9 +29,12 @@ export default tseslint.config(
   },
   {
     rules: {
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
