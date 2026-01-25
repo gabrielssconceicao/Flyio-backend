5cdd20213773faf66d99ed0d7fb5cdbfdb7dad40
Gabriel Conceição <gabrielc2019@outlook.com.br>
2026-01-25 10:46:30 -0300

    feat: create post entity

--- src/core/entities/unique-entity-id.spec.ts
+++ src/core/entities/unique-entity-id.spec.ts
@@ -10,4 +10,12 @@ describe('UniqueEntityId', () => {
     const id = new UniqueEntityId('123');
     expect(id.value).toEqual('123');
   });
+
+  it('should compare ids', () => {
+    const id1 = new UniqueEntityId('123');
+    const id2 = new UniqueEntityId('123');
+    expect(id1.equals(id2)).toBe(true);
+    expect(id1.equals('123')).toBe(true);
+    expect(id1.equals('124')).toBe(false);
+  });
 });
---
+++ src/domain/social/enterprise/entities/post.ts
@@ -0,0 +1,34 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+
+interface PostProps {
+  author_id: UniqueEntityId;
+  content: string;
+  created_at: Date;
+  updated_at?: Date;
+}
+
+export class Post extends Entity<PostProps> {
+  get author_id() {
+    return this.props.author_id;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+
+  get updated_at() {
+    return this.props.updated_at;
+  }
+  static create(props: Optional<PostProps, 'created_at'>, id?: UniqueEntityId) {
+    return new Post(
+      { ...props, created_at: props.created_at ?? new Date() },
+      id,
+    );
+  }
+}
